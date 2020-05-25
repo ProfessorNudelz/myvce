b4ca2170b98381e10fcad981f5c6a487fc1adf59
Jesse Lyon <jesse.a.lyon+github@gmail.com>
2020-05-25 12:52:35 +1000

    Added tables to myStudies and added font awesome

--- src/pages/myDashboard.js
+++ src/pages/myDashboard.js
@@ -5,6 +5,15 @@ import Layout from '../components/layout'
 function myDashboard() {
   return (
     <Layout>
+      <div className="alert alert-warning" role="alert">
+        <h2 className="alert-heading"><i class="fas fa-exclamation-triangle"></i> COVID-19 Update</h2>
+        <hr />
+        <h6>29/05/2020</h6>
+        <p>Exams dates have been annouced. Please refer <a href="https://vcaa.vic.edu.au/administration/Key-dates/Pages/VCE-exam-timetable.aspx" className="alert-link">here.</a></p>
+        <hr />
+        <h6>10/05/2002</h6>
+        <p>Changes to the study designs for 2020 have been made. Please refer <a href="https://vcaa.vic.edu.au/curriculum/vce/vce-study-designs/Pages/vce-study-designs.aspx" className="alert-link">here</a> to view changes.</p>
+      </div>
       <div className="jumbotron">
         <h1 className="display-4">Hello, <samp>firstName</samp>!</h1>
         <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
--- src/pages/myStudies.js
+++ src/pages/myStudies.js
@@ -5,6 +5,152 @@ function myStudies() {
   return (
     <Layout>
       <h1>myStudies</h1>
+      <hr />
+      <h3>Completed</h3>
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Year</th>
+            <th scope="col">Study</th>
+            <th scope="col">Unit</th>
+            <th scope="col">Result</th>
+            <th scope="col">GA1</th>
+            <th scope="col">GA2</th>
+            <th scope="col">GA3</th>
+            <th scope="col">Study Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">2018</th>
+            <td>Computing</td>
+            <td>1/2</td>
+            <td>S/S</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <th scope="row">2019</th>
+            <td>Business Management</td>
+            <td>1/2</td>
+            <td>S/S</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <th scope="row">2019</th>
+            <td>English</td>
+            <td>1/2</td>
+            <td>S/S</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <th scope="row">2019</th>
+            <td>Mathematics: General Mathematics</td>
+            <td>1/2</td>
+            <td>S/S</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <th scope="row">2019</th>
+            <td>Mathematics: Mathematical Methods</td>
+            <td>1/2</td>
+            <td>S/S</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <th scope="row">2019</th>
+            <td>Computing: Informatics</td>
+            <td>3/4</td>
+            <td>S/S</td>
+            <td>C</td>
+            <td>C</td>
+            <td>C</td>
+            <td>30</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h3>Undertaking</h3>
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Year</th>
+            <th scope="col">Study</th>
+            <th scope="col">Unit</th>
+            <th scope="col">Result</th>
+            <th scope="col">GA1</th>
+            <th scope="col">GA2</th>
+            <th scope="col">GA3</th>
+            <th scope="col">Study Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">2020</th>
+            <td>Business Management</td>
+            <td>3/4</td>
+            <td>/</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <th scope="row">2020</th>
+            <td>English</td>
+            <td>3/4</td>
+            <td>/</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <th scope="row">2020</th>
+            <td>Mathematics: General Mathematics</td>
+            <td>3/4</td>
+            <td>/</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <th scope="row">2020</th>
+            <td>Mathematics: Mathematical Methods</td>
+            <td>3/4</td>
+            <td>/</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <th scope="row">2020</th>
+            <td>Computing: Informatics</td>
+            <td>3/4</td>
+            <td>/</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+        </tbody>
+      </table>
     </Layout>
   )
 }
